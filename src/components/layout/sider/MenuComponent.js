5b879b9eb64b14db052156b8ed814892d0ea3430
cuongnh2k <cuongnh2k@gmail.com>
2023-12-21 15:19:09 +0700

    update

--- src/components/layout/sider/MenuComponent.js
+++ src/components/layout/sider/MenuComponent.js
@@ -1,4 +1,4 @@
-import {Badge, Menu, Space, theme} from "antd";
+import {Badge, Menu, theme} from "antd";
 import React from "react";
 import {BellOutlined, BulbOutlined, FormOutlined, UserOutlined} from "@ant-design/icons";
 import {useNavigate} from "react-router-dom";
@@ -8,86 +8,44 @@ const MenuComponent = () => {
     const {
         token: {colorBgContainer},
     } = theme.useToken();
+    const items = [
+        {
+            label: (<p onClick={() => navigate("/account")}>Tài khoản</p>),
+            key: '/account',
+            icon: (<UserOutlined onClick={() => navigate("/account")}/>),
+        },
+        {
+            label: (<p onClick={() => navigate("/")}>Đánh giá</p>),
+            key: '/',
+            icon: (<FormOutlined onClick={() => navigate("/")}/>),
+        },
+        {
+            label: (<p onClick={() => navigate("/propose")}>Đề xuất</p>),
+            key: '/propose',
+            icon: (<BulbOutlined onClick={() => navigate("/propose")}/>),
+        },
+        {
+            label: (<p onClick={() => navigate("/notify")}>Thông báo</p>),
+            key: '/notify',
+            icon: (<Badge
+                onClick={() => navigate("/notify")}
+                count={1}
+                overflowCount={999}
+                size="small"
+            >
+                <BellOutlined
+                    style={{
+                        color: colorBgContainer,
+                        fontSize: 20
+                    }}
+                    size={1}
+                />
+            </Badge>),
+        },
+    ]
 
     return (
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={[window.location.pathname]}>
-            <Menu.Item
-                key={"/account"}
-                onClick={() => navigate("/account")}
-            >
-                <UserOutlined
-                    style={{
-                        fontSize: 20
-                    }}
-                />
-                <Space
-                    style={{
-                        marginLeft: 10
-                    }}
-                >
-                    Tài khoản
-                </Space>
-            </Menu.Item>
-            <Menu.Item
-                key={"/"}
-                onClick={() => navigate("/")}
-            >
-                <FormOutlined
-                    style={{
-                        fontSize: 20
-                    }}
-                />
-                <Space
-                    style={{
-                        marginLeft: 10
-                    }}
-                >
-                    Đánh giá
-                </Space>
-            </Menu.Item>
-            <Menu.Item
-                key={"/propose"}
-                onClick={() => navigate("/propose")}
-            >
-                <BulbOutlined
-                    style={{
-                        fontSize: 20
-                    }}
-                />
-                <Space
-                    style={{
-                        marginLeft: 10
-                    }}
-                >
-                    Đề xuất
-                </Space>
-            </Menu.Item>
-            <Menu.Item
-                key={"/notify"}
-                onClick={() => navigate("/notify")}
-            >
-                <Badge
-                    count={1}
-                    overflowCount={999}
-                    size="small"
-                >
-                    <BellOutlined
-                        style={{
-                            color: colorBgContainer,
-                            fontSize: 20
-                        }}
-                        size={1}
-                    />
-                </Badge>
-                <Space
-                    style={{
-                        marginLeft: 10
-                    }}
-                >
-                    Thông báo
-                </Space>
-            </Menu.Item>
-        </Menu>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={[window.location.pathname]} items={items}/>
     )
 }
 export default MenuComponent
